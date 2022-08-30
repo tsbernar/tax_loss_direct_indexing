e0ddf2a397432ed6fb5e0ca9540dbecce4f82fc6
Trevor Bernard <trevor.s.bernard@gmail.com>
2022-08-30 18:08:30 -0500

    Fix BRK-B ticker from ibkr

--- scripts/get_ibkr_conids_for_tickers.py
+++ scripts/get_ibkr_conids_for_tickers.py
@@ -1,16 +1,21 @@
 import requests
 import click
 import pandas as pd
+import urllib.parse
 from typing import List
 
 TICKER_DATA_FILE_NAME = "yf_tickers.parquet"  # TODO move these to central constants file
 DEFAULT_DATA_DIR = "./data"
 BASE_URL = 'https://localhost:5000/v1/api/trsrv/stocks/?symbols='
 
+YF_TO_IBKR_TICKER_CORRECTION_MAP = {
+    "BRK-B": "BRK B",
+}
+
 
 def make_url(tickers: List[str]) -> str:
     url = BASE_URL + ",".join(tickers)
-    return url
+    return urllib.parse.quote(url)
 
 
 def process_response(tickers: List[str], r : requests.Response, filepath: str) -> None:
@@ -36,6 +41,7 @@ def process_response(tickers: List[str], r : requests.Response, filepath: str) -
 def main(filepath: str) -> None:
     df = pd.read_parquet(filepath + '/' + TICKER_DATA_FILE_NAME)
     tickers : List[str] = list(df.columns)
+    tickers = [t if t not in YF_TO_IBKR_TICKER_CORRECTION_MAP else YF_TO_IBKR_TICKER_CORRECTION_MAP[t] for t in tickers]
     url = make_url(tickers)
     r = requests.post(url, verify=False)
     if r.status_code != 200:
