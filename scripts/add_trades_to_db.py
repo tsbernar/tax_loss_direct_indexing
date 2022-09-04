9478557d0a3c78505f0c29e14c76736dded45c8c
Trevor Bernard <trevor.s.bernard@gmail.com>
2022-09-04 00:07:23 -0500

    .

--- scripts/add_trades_to_db.py
+++ scripts/add_trades_to_db.py
@@ -11,6 +11,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) 
 
+
 def build_check_query(execution_id: str, table_name: str) -> str:
     return f"SELECT execution_id from {table_name} WHERE execution_id='{execution_id}' LIMIT 1"
 
@@ -19,14 +20,24 @@ def build_insert_statement(table_name: str) -> str:
     return f"INSERT INTO {table_name} VALUES(?, ?)"
 
 
+def build_create_statement(table_name: str) -> str:
+    return f"CREATE TABLE {table_name}(execution_id UNIQUE, json)"
+
+
 @click.command()
 @click.option("--config", "config_file", required=True)
-@click.option("-v", "--verbose")
-def main(config_file, verbose):
+@click.option("-v", "--verbose", is_flag=True)
+@click.option("--create_table", is_flag=True)
+def main(config_file: str, verbose: bool, create_table: bool) -> None:
     config = munch.munchify(yaml.safe_load(open(config_file)))
+    table_name = config.database.ibkr_trades_json_table_name
 
     con = sqlite3.connect(config.database.file)
     cur = con.cursor()
+
+    if create_table:
+        cur.execute(build_create_statement(table_name))
+        con.commit()
 
     gw = IBKRGateway(config=config.gateway)
     ibkr_trades = gw.get_ibkr_trades()
@@ -36,11 +47,11 @@ def main(config_file, verbose):
         if verbose:
             print(trade)
         execution_id = trade["execution_id"]
-        cur.execute(build_check_query(execution_id, config.database.ibkr_trades_json_table_name))
+        cur.execute(build_check_query(execution_id, table_name))
         if cur.fetchone() is None:
             print(f"Inserting trade {trade}")
             cur.execute(
-                build_insert_statement(config.database.ibkr_trades_json_table_name), (execution_id, json.dumps(trade))
+                build_insert_statement(table_name), (execution_id, json.dumps(trade))
             )
             con.commit()
             inserted += 1
