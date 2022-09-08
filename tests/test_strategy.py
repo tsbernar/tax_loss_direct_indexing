a07edf56a41358ed3f432c0d6147603a95d6ef93
Trevor Bernard <trevor.s.bernard@gmail.com>
2022-09-08 15:21:03 -0500

    Add strategy init unit test

--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 
 import mock
 import munch
+import pandas as pd
 import pytest
 from freezegun import freeze_time
 
@@ -13,7 +14,25 @@ from tax_loss.trade import Side
 
 @pytest.fixture
 def config():
-    config = munch.Munch({"ticker_blacklist_extra": []})
+    config = munch.munchify(
+        {
+            "ticker_blacklist_extra": [],
+            "price_data_file": "filename",
+            "portfolio_file": "filename",
+            "ticker_blacklist_file": "filename",
+            "max_stocks": 100,
+            "index_weight_file": "filename",
+            "optimizer": {
+                "lookback_days": 10,
+                "tax_coefficient": 0.4,
+                "max_deviation_from_true_weight": 0.05,
+                "cash_constraint": 0.9,
+                "tracking_error_func": "least_squared",
+                "max_total_deviation": 0.5,
+            },
+            "gateway": {},
+        }
+    )
     return config
 
 
@@ -46,3 +65,26 @@ def test_plan_transactions():
         mock_self, desired_portfolio=desired, current_portfolio=current
     )
     assert len(transactions) == 0
+
+
+def test_init_strategy(config, monkeypatch):
+    monkeypatch.setattr(
+        DirectIndexTaxLossStrategy,
+        "_load_yf_prices",
+        lambda x, y, z: pd.DataFrame(
+            {"A": [12.1, 12.3], "AA": [13.1, 13.4], "IVV": [100.0, 100.1]},
+            index=pd.DatetimeIndex(["2006-09-29", "2006-10-02"], name="index"),
+        ),
+    )
+    monkeypatch.setattr(DirectIndexTaxLossStrategy, "_load_current_portfolio", lambda x, y: Portfolio(cash=100.0))
+    monkeypatch.setattr(DirectIndexTaxLossStrategy, "_load_ticker_blacklist", lambda x, y, z: {"A": None})
+    monkeypatch.setattr(
+        DirectIndexTaxLossStrategy, "_load_index_weights", lambda x, y, z: pd.Series([0.75, 0.25], index=["A", "AA"])
+    )
+    mock_gw = mock.Mock()
+    mock_gw.get_market_prices = lambda tickers: {
+        "A": MarketPrice(10.0, pd.Timestamp.now().to_pydatetime()),
+        "AA": MarketPrice(12.0, pd.Timestamp.now().to_pydatetime()),
+    }
+    monkeypatch.setattr(DirectIndexTaxLossStrategy, "_init_gateway", lambda x, y: mock_gw)
+    DirectIndexTaxLossStrategy(config)
